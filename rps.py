ca9ea92387d4ad9f36e46b5e5d941d19ba84e0bc
TomasAisti <tomasaristimuno@gmail.com>
2023-02-05 20:04:31 -0300

    C:/Program Files/Git/

--- rps.py
+++ rps.py
@@ -1,19 +1,41 @@
 import random
-while 1==1:
-    player1 = input("Select Rock, Paper, or Scissor :").lower()
-    player2 = random.choice(["Rock", "Paper", "Scissor"]).lower()
-    print("Player 2 selected: ", player2)
 
-    if player1 == "rock" and player2 == "paper":
-        print("Player 2 Won, better luck next time")
-    elif player1 == "paper" and player2 == "scissor":
-        print("Player 2 Won, better luck next time")
-    elif player1 == "scissor" and player2 == "rock":
-        print("Player 2 Won, better luck next time")
-    elif player1 == player2:
+Games = 0
+You = 0
+PC = 0
+
+while Games <= 10:
+    print("You:", You)
+    print("PC:", PC)
+    print("Games played: ", Games )
+
+    you = input("Select Rock, Paper, or Scissor :").lower()
+    pc = random.choice(["Rock", "Paper", "Scissor"]).lower()
+    print("PC:", pc)
+
+    if you == "rock" and pc == "paper":
+        print("PC won, better luck next time.")
+        PC = PC + 1
+        Games = Games +1
+    elif you == "paper" and pc == "scissor":
+        print("PC won, better luck next time.")
+        PC = PC + 1
+        Games = Games +1
+    elif you == "scissor" and pc == "rock":
+        print("PC won, better luck next time.")
+        PC = PC + 1
+        Games = Games +1
+    elif you == pc:
         print("Tie")
-    elif player1 != "scissor" or "paper" or "rock":
-        print("Cheater! Player 2 wins")
-    else:
-        print("Player 1 Won")
-    input("Thanks for playing. Press any key to start again.")
+        Games = Games +1
+
+    elif you != "rock"or"paper"or"scissor":
+        print("Invalid match.")
+        Games = Games +1
+
+    else:       
+        print("You win.")
+        You = You + 1
+        Games = Games +1
+
+
