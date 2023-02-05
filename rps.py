12746afa6e302e90d50d30bdca512620d8fd0653
TomasAisti <tomasaristimuno@gmail.com>
2023-02-05 18:08:35 -0300

    Anti-cheat system developed

--- rps.py
+++ rps.py
@@ -1,21 +1,19 @@
 import random
+while 1==1:
+    player1 = input("Select Rock, Paper, or Scissor :").lower()
+    player2 = random.choice(["Rock", "Paper", "Scissor"]).lower()
+    print("Player 2 selected: ", player2)
 
-You = input("Select Rock, Paper, or Scissor :").lower()
-PC = random.choice(["Rock", "Paper", "Scissor"]).lower()
-print("PC selected: ", PC)
-
-if You == "rock" and PC == "paper":
-    print("PC Won")
-elif You == "paper" and PC == "scissor":
-    print("PC Won")
-elif You == "scissor" and PC == "rock":
-    print("PC Won")
-elif You == PC:
-    print("Tie")
-elif You != "rock" or"paper" or"scissor":
-    print("Cheater, pc wins")
-else:
-    print("You Won")
-
-    
-    
+    if player1 == "rock" and player2 == "paper":
+        print("Player 2 Won, better luck next time")
+    elif player1 == "paper" and player2 == "scissor":
+        print("Player 2 Won, better luck next time")
+    elif player1 == "scissor" and player2 == "rock":
+        print("Player 2 Won, better luck next time")
+    elif player1 == player2:
+        print("Tie")
+    elif player1 != "scissor" or "paper" or "rock":
+        print("Cheater! Player 2 wins")
+    else:
+        print("Player 1 Won")
+    input("Thanks for playing. Press any key to start again.")
